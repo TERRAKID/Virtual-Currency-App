6f19d070e31f3962805b72fcf1c9c2553059659b
TERRAKID <wannes.verboven@outlook.com>
2020-05-17 12:47:24 +0200

    [feat] Add basic login and sign up form

--- app.js
+++ app.js
@@ -24,6 +24,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/stylesheets', express.static(__dirname + '/node_modules/normalize.css/'));
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api/v1/currency', apiCurrencyRouter);
